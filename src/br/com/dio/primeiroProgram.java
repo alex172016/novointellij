80af5f590bfafe5bc0ab758115d19e8a3b7255fa
Alex Santos <alex_santosabs@outlook.com>
2022-05-18 22:45:58 -0300

    novo commit

---
+++ src/br/com/dio/primeiroProgram.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+public class primeiroProgram {
+    public static void main(String[] args) {
+
+        int a =5;
+        int b =8;
+
+
+        System.out.println("hello world" +(a+b));
+
+    }
+}
